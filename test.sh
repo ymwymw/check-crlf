7188c8b2de184495c256498861de494864210b97
ymwymw <31081824+ymwymw@users.noreply.github.com>
2023-03-05 10:34:31 -0500

    testing

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ printf "\n" > "temp/good-lf"
 printf "\r\n" > "temp/good-crlf"
 printf $'\t' > "temp/tab"
 
-grep -rnI --exclude "*.x*" --exclude "*.*ML" --exclude "*.*ml" --exclude "*.Designer.*" --exclude "*.sql" --exclude "*.htm*" --exclude "*.*css" --exclude "*.txt" --exclude "*.resx" --exclude "*.wsdl" --exclude "*.aff" --exclude "*.sln" --exclude "*.designer.*" --exclude "*.config" --exclude "*.tab" --exclude "*.js" --exclude-dir "TestCode" --exclude-dir ".git" --exclude-dir "node_modules" --exclude-dir "bin" --exclude-dir "obj" --exclude-dir "MobileApp" --exclude-dir "GatewayTest" $'\t' "temp" > results.txt && cat results.txt
+grep -rnI --exclude "*.x*" --exclude "*.*ML" --exclude "*.*ml" --exclude "*.Designer.*" --exclude "*.sql" --exclude "*.htm*" --exclude "*.*css" --exclude "*.txt" --exclude "*.resx" --exclude "*.wsdl" --exclude "*.aff" --exclude "*.sln" --exclude "*.designer.*" --exclude "*.config" --exclude "*.tab" --exclude "*.js" --exclude-dir "TestCode" --exclude-dir ".git" --exclude-dir "node_modules" --exclude-dir "bin" --exclude-dir "obj" --exclude-dir "MobileApp" --exclude-dir "GatewayTest" $'\t' "temp" > results.txt && printf "%s" "$(<results.txt)"
 printf "Ran grep in test.sh"
 # ./entrypoint.sh . "temp/ignored"
 
